d7a26a5b0c119e414e1bdadf065a8aa7552b978a
Treznaut <68379713+Treznaut@users.noreply.github.com>
2022-04-12 14:37:41 +0100

    Delete Girlfriend.py

--- Girlfriend.py
+++
@@ -1,7 +0,0 @@
-gfname = input("Enter the name of your Girlfriend: ")
-name = input("Enter your name: ")
-
-print(f"Your girlfriends name is {gfname} and your name is {name}")
-
-print(f"{gfname} thinks {name} is hot UwU")
-print(f"{name} has very thicc thighs thinks {gfname}")
